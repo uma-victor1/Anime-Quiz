2ebb104d4697678c0201ef36fb151d408533ef76
umavictor <umavictor11@gmail.com>
2020-11-29 07:53:55 +0100

    Blank setup to start quiz app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <div id="app">
-    <Quiz />
   </div>
 </template>
 
 <script>
-import Quiz from './components/Quiz.vue'
-
 export default {
   name: 'App',
   components: {
-    Quiz
+  
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,30 +5,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    Loading: false,
-    posts: []
   },
   mutations: {
-    LOAD_POST: (state, data) => {
-      state.posts = data
-    },
-    START_LOADING (state) {
-      state.Loading = true
-    },
-    STOP_LOADING (state) {
-      state.Loading = false
-    }
   },
   actions: {
-    SET_STATUS: ({ commit }) => {
-      commit('START_LOADING')
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => {
-          commit('LOAD_POST', data)
-          commit('STOP_LOADING')
-        })
-    }
   },
   modules: {
   }
